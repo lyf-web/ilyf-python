8770f7824e928442dd4be3c8a241d22854b8c3b4
lyfei-web <120346013@qq.com>
2020-07-10 20:09:37 +0800

    Revert "自学python"
    This reverts commit 7e8bb9c19157210b7ad33abe257d5c972d5460ec.

--- 简单web服务器/application/utils.py
+++
@@ -1,9 +0,0 @@
-#-*-coding:utf-8-*-
-
-def create_http_response(status,response_body):
-    response_line = f'HTTP/1.0 {status}\r\n'
-    response_header = 'Server:Python_web_server/1.0\r\n'
-    response_header += 'Content-Type: text/html\r\n'
-    response_blank = '\r\n'
-    response_data = (response_line + response_header +response_blank).encode() + response_body
-    return response_data
