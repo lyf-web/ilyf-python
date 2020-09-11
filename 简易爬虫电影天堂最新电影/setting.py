335505aa28dc9e1e0426deea91e4de6bb8c92a31
lyfei-web <120346013@qq.com>
2020-09-11 15:56:28 +0800

    东方财富网个股信息爬虫

--- 简易爬虫电影天堂最新电影/setting.py
+++
@@ -1,6 +0,0 @@
-web_site = 'https://www.dy2018.com'   #设置需要爬取网站的域名
-web_site_dir = '/html/gndy/dyzz'
-sql_host = 'localhost'  #设置sql数据库地址
-sql_user = 'root'       #设置sql用户名
-sql_passwd = 'zy051201230'  #设置sql用户密码
-sql_database = 'movie_db'   #设置操作的数据库名
